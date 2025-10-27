cfcae55d3852beacb6104ba8594d874695e2571a
WaitHoldMyBeer <kenanblair2@gmail.com>
2025-10-26 22:40:15 -0700

    done

--- src/your_solution/src/spin_sol.cpp
+++ src/your_solution/src/spin_sol.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include "spin_sol.h"
 
 int main(int argc, char *argv[]) {
@@ -8,7 +9,30 @@ int main(int argc, char *argv[]) {
 }
 
 //your code here
-SpinSolution::SpinSolution() : Node("spinsolution") {
-  RCLCPP_INFO(this->get_logger(), "Remove this statement from spin_sol.cpp");
-  // your code here
+SpinSolution::SpinSolution() : Node("spinsolution") //Spinsolution constructor that extends node spin solution with the member "spinsolution"
+{
+	pos_subscription_ = this->create_subscription<ArrayMsg>("measuredpos", 10, std::bind(&SpinSolution::measuredpos_callback, this, std::placeholders::_1));
+	vel_subscription_ = this->create_subscription<ArrayMsg>("measuredvel", 10, std::bind(&SpinSolution::measuredvel_callback, this, std::placeholders::_1));
+	publisher_ = this->create_publisher<ArrayMsg>("predictedpos", 10);
+}
+
+void SpinSolution::measuredvel_callback(const ArrayMsg &msg)
+{
+	velx = msg.data[0];
+	vely = msg.data[1];
+}
+
+void SpinSolution::measuredpos_callback(const ArrayMsg &msg)
+{
+	RCLCPP_INFO(this->get_logger(), "New position!! (%f, %f)", msg.data[0], msg.data[1]);
+	pos_ = ArrayMsg();
+	pos_.data = msg.data;
+	timer_ = this->create_wall_timer(1ms, std::bind(&SpinSolution::timer_callback, this));
+}
+
+void SpinSolution::timer_callback()
+{
+	pos_.data[0] += velx/1000;
+	pos_.data[1] += vely/1000;
+	publisher_->publish(pos_);
 }
--- src/your_solution/src/spin_sol.h
+++ src/your_solution/src/spin_sol.h
@@ -4,11 +4,23 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float64_multi_array.hpp"
 using ArrayMsg = std_msgs::msg::Float64MultiArray;
+using std::placeholders::_1;
+using namespace std::chrono_literals;
 
 class SpinSolution : public rclcpp::Node {
  public:
   SpinSolution();
  private:
+   void measuredpos_callback(const ArrayMsg& msg);
+   void measuredvel_callback(const ArrayMsg &msg);
+   void timer_callback();   
+   rclcpp::Subscription<ArrayMsg>::SharedPtr pos_subscription_; //creates a subscriber member
+   rclcpp::Subscription<ArrayMsg>::SharedPtr vel_subscription_; //creates a subscriber member
+   rclcpp::Publisher<ArrayMsg>::SharedPtr publisher_; //creates a publisher member
+   rclcpp::TimerBase::SharedPtr timer_;
+   float velx;
+   float vely;
+   ArrayMsg pos_;
     // your code here
 };
 
